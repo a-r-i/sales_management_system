97955714554af472ac34fb6d9f6a38662b692129
ari <nemuriobjet@gmail.com>
2019-02-03 21:29:50 +0900

    Fruitモデル, Saleモデルを定義

--- sales_management_system/models.py
+++ sales_management_system/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Fruit(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)  # 登録日時
+    updated_at = models.DateTimeField(auto_now=True)  # 更新日時
+
+    def __str__(self):
+        return self.name
+
+
+class Sale(models.Model):
+    fruit = models.ForeignKey(Fruit, on_delete=models.PROTECT)
+    amount = models.IntegerField()
+    total_price = models.IntegerField()  # 合計金額
+    sold_at = models.DateTimeField()  # 販売日時
