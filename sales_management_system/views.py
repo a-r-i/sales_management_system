784de223cfaf9e73ea69c7079e5a48f7bb3d1799
ari <nemuriobjet@gmail.com>
2019-02-03 23:52:00 +0900

    FruitListViewを実装

--- sales_management_system/views.py
+++ sales_management_system/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Fruit
+
+
+class FruitListView(ListView):
+
+    model = Fruit
