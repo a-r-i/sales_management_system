162834666de798f7e91b8ffa95dd677a9c9ea35c
ari <nemuriobjet@gmail.com>
2019-02-04 16:14:01 +0900

    local_settingsをcofigディレクトリに移動

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-from local_settings import SECRET_KEY, DEBUG
+from .local_settings import SECRET_KEY, DEBUG
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
