a5798cee9c7726f78d0dc6f4bf920a4e1b2eafb0
ari <nemuriobjet@gmail.com>
2019-02-04 22:49:29 +0900

    果物マスタ管理 削除機能を実装

--- sales_management_system/urls.py
+++ sales_management_system/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('fruit-list', views.FruitListView.as_view(), name='fruit_list'),
     path('create-fruit', views.FruitCreateView.as_view(), name='create_fruit'),
     path('update-fruit/<int:pk>', views.FruitUpdateView.as_view(), name='update_fruit'),
+    path('delete-fruit/<int:pk>', views.FruitDeleteView.as_view(), name='delete_fruit'),
 ]
--- sales_management_system/views.py
+++ sales_management_system/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .forms import FruitForm
 from .models import Fruit
@@ -24,4 +24,11 @@ class FruitUpdateView(UpdateView):
     model = Fruit
     form_class = FruitForm
     template_name = "sales_management_system/fruit_form.html"
+    success_url = "/fruit-list"
+
+
+class FruitDeleteView(DeleteView):
+
+    model = Fruit
+    template_name = "sales_management_system/fruit_confirm_delete.html"
     success_url = "/fruit-list"
