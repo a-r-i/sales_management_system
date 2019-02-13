78e47f2d8cf00b7e1eeccb0168e3ab36bacddcc3
ari <nemuriobjet@gmail.com>
2019-02-14 03:04:30 +0900

    services.py リファクタリング

--- sales_management_system/services.py
+++ sales_management_system/services.py
@@ -23,40 +23,51 @@ def aggregate_sales_information(date_type, number):
 
     for i in range(1, number + 1):
         if date_type == 'month':
-            today_aggregate_month = today + relativedelta(months=-i)
-            first_day_aggregate_month = today_aggregate_month.replace(day=1)
-            last_day_aggregate_month = first_day_aggregate_month + relativedelta(months=1, days=-1)
+            this_day_aggregate_month = today + relativedelta(months=-i)  # 統計を取る月の今日
+            first_day_aggregate_month = this_day_aggregate_month.replace(day=1)  # 統計を取る月の1日
+            last_day_aggregate_month = first_day_aggregate_month + relativedelta(months=1, days=-1)  # 統計を取る月の最終日
+
+            sale_objects_aggregate_month = Sale.objects.filter(sold_at__range=[
+                                                                               first_day_aggregate_month,
+                                                                               last_day_aggregate_month
+                                                                               ])
+
             aggregate_date = '%i年%i月' % (first_day_aggregate_month.year, first_day_aggregate_month.month)
-            sale_objects = Sale.objects.filter(sold_at__range=[first_day_aggregate_month, last_day_aggregate_month])
-            revenue = aggregate_revenue(sale_objects)
-            detail = aggregate_detail(sale_objects)
+            revenue = aggregate_revenue(sale_objects_aggregate_month)
+            detail = aggregate_detail(sale_objects_aggregate_month)
+
             sales_information.append({'date': aggregate_date, 'revenue': revenue, 'detail': detail})
         elif date_type == 'day':
-            aggregate_date = today + timedelta(days=-i)
-            sale_objects = Sale.objects.filter(sold_at__date=aggregate_date)
-            revenue = aggregate_revenue(sale_objects)
-            detail = aggregate_detail(sale_objects)
+            aggregate_date = today + timedelta(days=-i)  # 統計を取る日付
+
+            sale_objects_aggregate_date = Sale.objects.filter(sold_at__date=aggregate_date)
+
+            revenue = aggregate_revenue(sale_objects_aggregate_date)
+            detail = aggregate_detail(sale_objects_aggregate_date)
+
             sales_information.append({'date': aggregate_date, 'revenue': revenue, 'detail': detail})
 
     return sales_information
 
 
 def aggregate_detail(sale_objects):
-    daily_detail_dict = {}
+    detail_dict = {}
 
+    # 「同じ果物の販売情報」をひとつに合算し、以下のような二次元辞書をつくる
+    # {'ブルーベリー': {'revenue': 200, 'amount': 4}, 'メロン': {'revenue': 20200, 'amount': 8}}
     for obj in sale_objects:
         fruit_name = str(obj.fruit)
-        if fruit_name in daily_detail_dict.keys():
-            daily_detail_dict[fruit_name]['revenue'] += obj.revenue
-            daily_detail_dict[fruit_name]['amount'] += obj.amount
+        if fruit_name in detail_dict.keys():
+            detail_dict[fruit_name]['revenue'] += obj.revenue
+            detail_dict[fruit_name]['amount'] += obj.amount
         else:
-            daily_detail_dict[fruit_name] = {}
-            daily_detail_dict[fruit_name]['revenue'] = obj.revenue
-            daily_detail_dict[fruit_name]['amount'] = obj.amount
+            detail_dict[fruit_name] = {}
+            detail_dict[fruit_name]['revenue'] = obj.revenue
+            detail_dict[fruit_name]['amount'] = obj.amount
 
-    daily_detail = ''
+    detail = ''
 
-    for key, value in daily_detail_dict.items():
-        daily_detail += '%s:%i円(%i)' % (key, value['revenue'], value['amount'])
+    for key, value in detail_dict.items():
+        detail += '%s:%i円(%i)' % (key, value['revenue'], value['amount'])
 
-    return daily_detail
+    return detail
