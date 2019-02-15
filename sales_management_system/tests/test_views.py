74d4ff13bd3c1116b2eea5a14bc7ff45b95fc842
ari <nemuriobjet@gmail.com>
2019-02-15 18:34:46 +0900

    update test_views.py

--- sales_management_system/tests/test_views.py
+++ sales_management_system/tests/test_views.py
@@ -9,7 +9,7 @@ class TestLogin(TestCase):
         self.assertTemplateUsed(response, 'sales_management_system/login.html')
 
 
-class TestIndexView(TestCase):
+class TestTopView(TestCase):
     def setUp(cls):
         User.objects.create_user(username='testuser',
                                  email='test@test.com',
@@ -28,3 +28,108 @@ class TestIndexView(TestCase):
         response = self.client.get('/')
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'sales_management_system/index.html')
+
+
+class TestFruitListView(TestCase):
+    def setUp(cls):
+        User.objects.create_user(username='testuser',
+                                 email='test@test.com',
+                                 password='password',
+                                 last_name='test',
+                                 first_name='user')
+
+    # ログインせず、ログインページ以外のページにアクセスした場合、ログインページへリダイレクト
+    def test_not_authenticated_get(self):
+        response = self.client.get('/fruit-list/')
+        self.assertRedirects(response, '/login/?redirect_to=/fruit-list/', status_code=302,
+                             target_status_code=200, fetch_redirect_response=True)
+
+    def test_authenticated_get(self):
+        self.client.login(username='testuser', password='password')
+        response = self.client.get('/fruit-list/')
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'sales_management_system/fruit_list.html')
+
+
+class TestFruitCreateView(TestCase):
+    def setUp(cls):
+        User.objects.create_user(username='testuser',
+                                 email='test@test.com',
+                                 password='password',
+                                 last_name='test',
+                                 first_name='user')
+
+    # ログインせず、ログインページ以外のページにアクセスした場合、ログインページへリダイレクト
+    def test_not_authenticated_get(self):
+        response = self.client.get('/create-fruit/')
+        self.assertRedirects(response, '/login/?redirect_to=/create-fruit/', status_code=302,
+                             target_status_code=200, fetch_redirect_response=True)
+
+    def test_authenticated_get(self):
+        self.client.login(username='testuser', password='password')
+        response = self.client.get('/create-fruit/')
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'sales_management_system/fruit_form.html')
+
+
+class TestSaleManagementView(TestCase):
+    def setUp(cls):
+        User.objects.create_user(username='testuser',
+                                 email='test@test.com',
+                                 password='password',
+                                 last_name='test',
+                                 first_name='user')
+
+    # ログインせず、ログインページ以外のページにアクセスした場合、ログインページへリダイレクト
+    def test_not_authenticated_get(self):
+        response = self.client.get('/sale-management/')
+        self.assertRedirects(response, '/login/?redirect_to=/sale-management/', status_code=302,
+                             target_status_code=200, fetch_redirect_response=True)
+
+    def test_authenticated_get(self):
+        self.client.login(username='testuser', password='password')
+        response = self.client.get('/sale-management/')
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'sales_management_system/sale_management.html')
+
+
+class TestSaleFormView(TestCase):
+    def setUp(cls):
+        User.objects.create_user(username='testuser',
+                                 email='test@test.com',
+                                 password='password',
+                                 last_name='test',
+                                 first_name='user')
+
+    # ログインせず、ログインページ以外のページにアクセスした場合、ログインページへリダイレクト
+    def test_not_authenticated_get(self):
+        response = self.client.get('/sale-form/')
+        self.assertRedirects(response, '/login/?redirect_to=/sale-form/', status_code=302,
+                             target_status_code=200, fetch_redirect_response=True)
+
+    def test_authenticated_get(self):
+        self.client.login(username='testuser', password='password')
+        response = self.client.get('/sale-form/')
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'sales_management_system/sale_form.html')
+
+
+class TestSaleStatisticsView(TestCase):
+    def setUp(cls):
+        User.objects.create_user(username='testuser',
+                                 email='test@test.com',
+                                 password='password',
+                                 last_name='test',
+                                 first_name='user')
+
+    # ログインせず、ログインページ以外のページにアクセスした場合、ログインページへリダイレクト
+    def test_not_authenticated_get(self):
+        response = self.client.get('/sale-statistics/')
+        self.assertRedirects(response, '/login/?redirect_to=/sale-statistics/', status_code=302,
+                             target_status_code=200, fetch_redirect_response=True)
+
+    def test_authenticated_get(self):
+        self.client.login(username='testuser', password='password')
+        response = self.client.get('/sale-statistics/')
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'sales_management_system/sale_statistics.html')
