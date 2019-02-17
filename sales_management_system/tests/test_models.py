6c014e514c0b2dc30b2357efc02a041198c2b4be
ari <nemuriobjet@gmail.com>
2019-02-17 23:07:28 +0900

    テスト追加「販売情報の売り上げは、販売情報登録後に果物の単価が変更されても、金額が変更されずそのままであるか」

--- sales_management_system/tests/test_models.py
+++ sales_management_system/tests/test_models.py
@@ -1,5 +1,30 @@
-class TestSale():
-    def update_not_change_price(self):
+from django.test import TestCase
+
+from ..models import Fruit, Sale
+
+
+class TestSale(TestCase):
+    fixtures = ['test_models.json']
+    
+    def update_fruit_price_not_change_revenue(self):
         """
             販売情報の売り上げは、販売情報登録後に果物の単価が変更されても、金額が変更されずそのままであるか検証
         """
+        sale = Sale.objects.get(id=1)
+
+        # 果物単価変更前の売り上げ
+        before_revenue = sale.revenue
+
+        # 果物の単価を変更
+        fruit = Fruit.objects.get(id=sale.fruit.id)
+        before_fruit_price = fruit.price
+        print(before_fruit_price)
+        fruit.price = before_fruit_price + 1
+        fruit.save()
+        
+        sale.refresh_from_db()
+
+        # 果物単価変更後の売り上げ
+        after_revenue = sale.revenue
+        
+        self.assertEqual(before_revenue, after_revenue)
