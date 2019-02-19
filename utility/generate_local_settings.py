4e9f82a04760ead3c61144f3f0bf1fa63196680c
ari <nemuriobjet@gmail.com>
2019-02-19 23:10:52 +0900

    update generate_local_settings.py

--- utility/generate_local_settings.py
+++ utility/generate_local_settings.py
@@ -2,4 +2,8 @@ from django.core.management.utils import get_random_secret_key
 
 secret_key = get_random_secret_key()
 text = 'SECRET_KEY = \'{0}\'\nDEBUG = True'.format(secret_key)
-print(text)
+
+path = '../config/local_settings.py'
+
+with open(path, mode='w') as f:
+    f.write(text)
